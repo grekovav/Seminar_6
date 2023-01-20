d4045a7d05ae47af52c0a55c84540e47fadd7b4a
Artem <grekartemvl@mail.ru>
2023-01-20 14:49:28 +0500

    Add Task40

---
+++ Task40/Program.cs
@@ -0,0 +1,38 @@
+﻿/* 40. Напишите программу, которая принимает на вход три числа
+и проверяет, может ли существовать треугольник со сторонами
+такой длины.
+Теорема о неравенстве треугольника: каждая сторона
+треугольника меньше суммы двух других сторон.  */
+int getUserData(string message)
+{
+    Console.ForegroundColor = ConsoleColor.DarkCyan;
+    Console.WriteLine(message);
+    Console.ResetColor();
+    int userData = int.Parse(Console.ReadLine()!);
+    return userData;
+}
+bool IsTriangleExist(int a, int b, int c)
+{
+    bool isExist = false;
+    if (a < b + c && b < c + a && c < b + a)
+    {
+        isExist = true;
+    }
+    return isExist;
+}
+int a = getUserData("Введите длину стороны A");
+int b = getUserData("Введите длину стороны B");
+int c = getUserData("Введите длину стороны C");
+bool isExist = IsTriangleExist(a, b, c);
+Console.WriteLine($"Треугольник со сторонами [{a}, {b}, {c}] {((isExist)?"существует":"не существует")}");
+
+/*string str = (isExist)?"существует":"не существует";
+
+if (isExist)
+{
+    str = "существует";
+}
+else
+{
+    str = "не существует";
+}  */
