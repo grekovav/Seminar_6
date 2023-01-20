c1e181d4fc0f6f6f6d3d1d23564ef9996cdc9c6f
Artem <grekartemvl@mail.ru>
2023-01-20 17:44:52 +0500

    Add Task45

---
+++ Task45/Program.cs
@@ -0,0 +1,45 @@
+﻿/* 45. Напишите программу, которая будет создавать копию
+заданного массива с помощью поэлементного копирования.  */
+int[] generateArray(int length, int start, int end)
+{
+    int[] result = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        result[i] = new Random().Next(start, end + 1);
+    }
+    return result;
+}
+int getIntFromUser(string message)
+{
+    Console.WriteLine(message);
+    int userInt = int.Parse(Console.ReadLine()!);
+    return userInt;
+}
+int[] copyArray(int[] array)
+{
+    int[] result = new int[array.Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        result[i] = array[i];
+    }
+    return result;
+}
+void printArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+        if (i < array.Length - 1)
+        {
+            Console.Write(", ");
+        }
+    }
+    Console.WriteLine("]");
+}
+int length = getIntFromUser("Введите количество элементов массива");
+int[] array = generateArray(length, -10, 10);
+printArray(array);
+int[] result = copyArray(array);
+printArray(result);
+
