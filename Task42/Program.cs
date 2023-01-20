19aa892246ec4422d4c5d7b648f184ee854b3cb7
Artem <grekartemvl@mail.ru>
2023-01-20 16:58:03 +0500

    Add Task42

---
+++ Task42/Program.cs
@@ -0,0 +1,23 @@
+﻿/* 42. Напишите программу, которая будет преобразовывать
+десятичное число в двоичное. 45 -> 101101, 2 -> 10.   */
+int getUserData(string message)
+{
+    Console.ForegroundColor = ConsoleColor.DarkCyan;
+    Console.WriteLine(message);
+    Console.ResetColor();
+    int userData = int.Parse(Console.ReadLine()!);
+    return userData;
+}
+string TransformBinary(int number)
+{
+    string result = String.Empty;
+    while (number > 0)
+    {
+        result = number % 2 + result;
+        number = number / 2;
+    }
+    return result;
+}
+int number = getUserData("Введите число");
+string binary = TransformBinary(number);
+Console.WriteLine($"Число {number} в двоичной системе = {binary}");
